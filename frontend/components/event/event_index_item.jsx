a6f40798bf17443f34a50bbd163d788029aadd6d
psia12345 <peiksia@gmail.com>
2016-11-08 09:46:03 -0800

    include dateFormat

--- frontend/components/event/event_index_item.jsx
+++ frontend/components/event/event_index_item.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
-// import dateFormat from 'dateFormat';
+import dateFormat from 'dateFormat';
 
 const EventIndexItem = ({event}) =>(
   <div className="single-event">
     <img src={event.image_url} className="event-image-icon"/>
     <div className="event-info">
       <h2>{event.title}</h2>
+      <span>{dateFormat(event.start_date, "dddd, mmmm dS, yyyy")}</span>
     </div>
   </div>
 )
 
 export default EventIndexItem;
 
-// <span>{dateFormat(event.start_date, "dddd, mmmm dS, yyyy")}</span>
 
 // const PokemonIndexItem = ({ pokemon, router }) => {
 //   const handleClick = url => e => router.push(url);
